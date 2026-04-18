49a99b59ee86680958862c8f3b0d734f0becb104
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-18 19:28:00 +0400

    update dbcontext

--- SabakiumAPI/Data/AppDbContext.cs
+++ SabakiumAPI/Data/AppDbContext.cs
@@ -8,6 +8,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<User> Users => Set<User>();
     public DbSet<Post> Posts => Set<Post>();
     public DbSet<ChatMessage> ChatMessages => Set<ChatMessage>();
+    public DbSet<PostLike> PostLikes => Set<PostLike>();
 
     protected override void OnModelCreating(ModelBuilder b)
     {
@@ -17,6 +18,19 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             .HasOne(p => p.User)
             .WithMany(u => u.Posts)
             .HasForeignKey(p => p.UserId);
+
+        b.Entity<PostLike>()
+            .HasKey(l => new { l.PostId, l.UserId });
+
+        b.Entity<PostLike>()
+            .HasOne(l => l.Post)
+            .WithMany(p => p.Likes)
+            .HasForeignKey(l => l.PostId);
+
+        b.Entity<PostLike>()
+            .HasOne(l => l.User)
+            .WithMany(u => u.PostLikes)
+            .HasForeignKey(l => l.UserId);
 
         b.Entity<ChatMessage>()
             .HasOne(m => m.Sender)

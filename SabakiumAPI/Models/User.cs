6921eb31c7ed2501f3de5daab007320d8f30943c
sabaka-chabaka <gamekimg360@gmail.com>
2026-04-18 13:24:45 +0400

    update model

--- SabakiumAPI/Models/User.cs
+++ SabakiumAPI/Models/User.cs
@@ -6,6 +6,7 @@ public class User
     public string Username { get; set; } = "";
     public string DisplayName { get; set; } = "";
     public string PasswordHash { get; set; } = "";
+    public string? AvatarPath { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public ICollection<Post> Posts { get; set; } = [];
 }
